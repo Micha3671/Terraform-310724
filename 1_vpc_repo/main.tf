1bbe780e37d160de7c85f1feda5c359a39e1c868
Micha <michael.rigas@t-online.de>
2024-07-31 11:40:56 +0200

    update main.tf & terraform.yml

--- 1_vpc_repo/main.tf
+++ 1_vpc_repo/main.tf
@@ -9,3 +9,21 @@ resource "aws_vpc" "main" {
     Name = "main-vpc"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  
+  tags = {
+    Name = "subnet2"
+  }
+}
